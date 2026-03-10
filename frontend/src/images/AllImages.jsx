123c9d186e8eba43eb5b10cd4f62deda05f6c6f9
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-03-09 22:18:16 -0700

    Added React Router

--- frontend/src/images/AllImages.jsx
+++ frontend/src/images/AllImages.jsx
@@ -4,11 +4,11 @@ import { fetchAll } from "./ImageFetcher.js";
 import { ImageGrid } from "./ImageGrid.jsx";
 
 export function AllImages() {
-    const [imageData, _setImageData] = useState(fetchAll);
-    return (
-        <MainLayout>
-            <h2>All Images</h2>
-            <ImageGrid images={imageData} />
-        </MainLayout>
-    );
+  const [imageData, _setImageData] = useState(fetchAll);
+  return (
+    <>
+      <h2>All Images</h2>
+      <ImageGrid images={imageData} />
+    </>
+  );
 }
--- frontend/src/images/ImageDetails.jsx
+++ frontend/src/images/ImageDetails.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react";
+import { useParams } from "react-router";
 import { MainLayout } from "../MainLayout.jsx";
 import { fetchById } from "./ImageFetcher.js";
 
-export function ImageDetails({ imageId }) {
-    const [image, _setImage] = useState(fetchById(imageId));
-    if (!image) {
-        return <MainLayout><h2>Image not found</h2></MainLayout>;
-    }
+export function ImageDetails() {
+  const { imageId } = useParams();
 
-    return (
-        <MainLayout>
-            <h2>{image.name}</h2>
-            <p>By {image.author.username}</p>
-            <img className="ImageDetails-img" src={image.src} alt={image.name} />
-        </MainLayout>
-    )
+  const [image, _setImage] = useState(fetchById(imageId));
+
+  if (!image) {
+    return <h2>Image not found</h2>;
+  }
+
+  return (
+    <>
+      <h2>{image.name}</h2>
+      <p>By {image.author.username}</p>
+      <img className="ImageDetails-img" src={image.src} alt={image.name} />
+    </>
+  );
 }
