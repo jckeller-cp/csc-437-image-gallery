ce9890d452981267c09a784b530d9718b76e768e
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-03-09 22:47:02 -0700

    Added backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,16 @@ import { ImageDetails } from "./images/ImageDetails.jsx";
 import { UploadPage } from "./UploadPage.jsx";
 import { LoginPage } from "./LoginPage.jsx";
 import { MainLayout } from "./MainLayout.jsx";
+import { VALID_ROUTES } from "../../backend/src/shared/ValidRoutes.js";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<AllImages />} />
-        <Route path="images/:imageId" element={<ImageDetails />} />
-        <Route path="upload" element={<UploadPage />} />
-        <Route path="login" element={<LoginPage />} />
+        <Route path={VALID_ROUTES.SINGLE_IMAGE} element={<ImageDetails />} />
+        <Route path={VALID_ROUTES.UPLOAD} element={<UploadPage />} />
+        <Route path={VALID_ROUTES.LOGIN} element={<LoginPage />} />
       </Route>
     </Routes>
   );
---
+++ shared/ValidRoutes.js
@@ -0,0 +1,6 @@
+export const VALID_ROUTES = {
+  HOME: "/",
+  LOGIN: "/login",
+  UPLOAD: "/upload",
+  SINGLE_IMAGE: "/images/:imageId",
+};
