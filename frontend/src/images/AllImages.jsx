00a1533b47be6e5604bc599142a12d87339a561c
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-03-09 23:18:01 -0700

    Images are now fetched from backend

--- frontend/src/images/AllImages.jsx
+++ frontend/src/images/AllImages.jsx
@@ -1,10 +1,40 @@
-import { useState } from "react";
-import { MainLayout } from "../MainLayout.jsx";
-import { fetchAll } from "./ImageFetcher.js";
+import { useState, useEffect } from "react";
 import { ImageGrid } from "./ImageGrid.jsx";
 
 export function AllImages() {
-  const [imageData, _setImageData] = useState(fetchAll);
+  const [imageData, setImageData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("/api/images");
+        if (!response.ok) {
+          throw new Error(
+            `Error: HTTP ${response.status} ${response.statusText}`,
+          );
+        }
+        const data = await response.json();
+        setImageData(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <>
       <h2>All Images</h2>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Forward all requests from localhost:5173/api/* to localhost:3000/api/*
+      "/api": "http://localhost:3000",
+    },
+  },
+});
